f2b7412c98a92c1bed8bd4aa8feb3532d5c18575
Krishna Bhagat <krishnabhagat06@gmail.com>
2021-04-19 12:22:30 +0530

    Middlewares

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require('express')
 const PORT = 5000
 const app = express()
 
+const middleWare = (req,res,next)=>
+{
+    console.log("Middleware Executed")
+    next()
+}
+
+app.get('/home',middleWare,(req,res)=>{
+    res.send("Home Page")
+    console.log("Roue to home page")
+})
 app.get('/',(req,res)=>{
     res.send("Hello World")
+    console.log("Roue to /")
 })
 
 app.listen(PORT,()=>
