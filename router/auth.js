17922e20165d2740681fd8ae6ae5b92055dad009
Krishna Bhagat <krishnabhagat06@gmail.com>
2021-04-21 16:25:47 +0530

    Sigin Route

--- router/auth.js
+++ router/auth.js
@@ -2,6 +2,8 @@ const express = require('express')
 const route = express.Router();
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
+const bcrypt = require('bcryptjs');
+const { Router } = require('express');
 
 route.get('/',(req,res)=>
 {
@@ -19,24 +21,56 @@ route.post('/about',(req,res)=>
     {
         if(savedUser)
         return res.status(404).json({"Error":"User already exist"})
-        const user = new User({
-            email,
-            password,
-            name
+        bcrypt.hash(password,12)
+        .then((hashedpassword)=>
+        {
+            const user = new User({
+                email,
+                password:hashedpassword,
+                name
+            })
+            user.save()
+            .then(user=>
+                {
+                    res.json({message:"Saved Sucessfully"})
+                })
+                .catch(err=>{
+                    console.log(err);
+                })
+
         })
-        user.save()
-        .then(user=>
-            {
-                res.json({message:"Saved Sucessfully"})
-            })
-            .catch(err=>{
-                console.log(err);
-            })
+        
     })
     .catch(err=>{
         console.log(err);
     })
     
+})
+
+route.post('/signin',(req,res)=>
+{
+    const {name,email,password} = req.body
+    if(!email || !password)
+    return res.status(404).json({error:"please enter email and password"})
+    User.findOne({email:email})
+    .then(saveduser=>
+        {
+            if(!saveduser)
+            return res.status(404).json({error:"Invalid email or password"})
+            bcrypt.compare(password,saveduser.password)
+            .then(domatch=>{
+                if(domatch)
+                res.json({sucefull:"You are logged in"})
+                else
+                res.status(404).json({error:"Invalid email or passward"})
+            }).catch(err=>
+                {
+                    console.log(err);
+                })
+        }).catch(err=>
+            {
+                console.log(err);
+            })
 })
 
 module.exports = route;
