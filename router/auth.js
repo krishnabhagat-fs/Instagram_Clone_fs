43ed1887c1a223f20ed2454b286e7b36040810d2
Krishna Bhagat <krishnabhagat06@gmail.com>
2021-04-21 13:18:19 +0530

    mongo coonection check

--- router/auth.js
+++ router/auth.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const route = express.Router();
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
 
 route.get('/',(req,res)=>
 {
@@ -8,12 +10,33 @@ route.get('/',(req,res)=>
 
 route.post('/about',(req,res)=>
 {
-    console.log(req.body);
+    //console.log(req.body);
     const {name,email,password} = req.body
     if(!name || !email || !password)
-    res.status(401).json({"Error":"Please fill all the fields"})
-    else
-    res.json({"Sucesfull":"value passed"})
+    return res.status(404).json({"Error":"Please fill all the fields"})
+    User.findOne({email:email})
+    .then((savedUser)=>
+    {
+        if(savedUser)
+        return res.status(404).json({"Error":"User already exist"})
+        const user = new User({
+            email,
+            password,
+            name
+        })
+        user.save()
+        .then(user=>
+            {
+                res.json({message:"Saved Sucessfully"})
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+    
 })
 
 module.exports = route;
