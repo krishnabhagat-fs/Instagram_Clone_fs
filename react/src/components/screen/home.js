1f0b876d0d9f13f1e73dd696ae1b73b1a182fb47
Krishna Bhagat <krishnabhagat06@gmail.com>
2021-04-30 14:12:55 +0530

    creating homepage post UI

--- react/src/components/screen/home.js
+++ react/src/components/screen/home.js
@@ -3,15 +3,53 @@ import React from 'react'
 const Home = ()=>
 {
     return(
-    <div>
-        <div>
-            <div>
-                <img src="https://images.unsplash.com/photo-1593692667732-d53fdaec0b60?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fHBlcnNvbnxlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
-                style={{height:"120px" }} alt="This is profile "
-                />
+    <div className="home">
+        <div className="card home-card">
+            <h5>User Name</h5>
+            <div className="card-image">
+            <img src="https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmFja2dyb3VuZHxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"/>
             </div>
-            <div>
-
+            <div className="card-content">
+            <i className="material-icons heart-red">favorite</i>
+                <h6>Title</h6>
+                <p>This is amazing post</p>  
+                <input/>              
+            </div>
+        </div>
+        <div className="card home-card">
+            <h5>User Name</h5>
+            <div className="card-image">
+            <img src="https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmFja2dyb3VuZHxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"/>
+            </div>
+            <div className="card-content">
+            <i className="material-icons heart-red">favorite</i>
+                <h6>Title</h6>
+                <p>This is amazing post</p>  
+                <input/>              
+            </div>
+        </div>
+        <div className="card home-card">
+            <h5>User Name</h5>
+            <div className="card-image">
+            <img src="https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmFja2dyb3VuZHxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"/>
+            </div>
+            <div className="card-content">
+            <i className="material-icons heart-red">favorite</i>
+                <h6>Title</h6>
+                <p>This is amazing post</p>  
+                <input/>              
+            </div>
+        </div>
+        <div className="card home-card">
+            <h5>User Name</h5>
+            <div className="card-image">
+            <img src="https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmFja2dyb3VuZHxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"/>
+            </div>
+            <div className="card-content">
+            <i className="material-icons heart-red">favorite</i>
+                <h6>Title</h6>
+                <p>This is amazing post</p>  
+                <input/>              
             </div>
         </div>
     </div>
